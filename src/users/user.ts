c459827639d4a7a400d14017bccb463b8b3bfb89
mathis juret-rafin <mathis.juret-rafin@edu.devinci.fr>
2024-03-05 11:11:07 +0100

    20 passed

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,14 +1,22 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_USER_PORT } from "../config";
+import { BASE_USER_PORT, BASE_ONION_ROUTER_PORT } from "../config";
+import {createRandomSymmetricKey, symEncrypt, rsaEncrypt, exportSymKey} from "@/src/crypto";
+import {GetNodeRegistryBody, Node} from "@/src/registry/registry";
+
 
 export type SendMessageBody = {
   message: string;
   destinationUserId: number;
 };
 
+export interface Registry{
+  nodes : Node[];
+}
+
 let lastReceivedMessage: string| null = null;
 let lastSentMessage: string| null = null;
+let lastCircuit: Node[]=[];
 
 export async function user(userId: number) {
   const _user = express();
@@ -28,7 +36,17 @@ export async function user(userId: number) {
     res.json({result: lastSentMessage});
   });
 
+  _user.get("/getLastCircuit",(req,res)=>{
+    res.json({result: lastCircuit.map((node)=> node.nodeId)});
+  });
 
+  _user.post("/message", (req, res) => {
+    const message = req.body.message;
+
+    lastReceivedMessage = message;
+
+    res.status(200).send("success");
+  });
 
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
